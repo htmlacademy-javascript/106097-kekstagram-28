66e5d3957cf356a270d10c573503d045b99523c4
Nikolay Stebunov <ponochevnii@mail.ru>
2023-03-25 21:47:32 +0300

    fix review issues

--- js/form.js
+++ js/form.js
@@ -2,11 +2,11 @@ import {isEscapeKey, isEnterKey} from './util.js';
 
 const MAX_HASHTAGS_AMOUNT = 5;
 const MAX_DESCRIPTION_LENGTH = 140;
+const REGEXP = new RegExp('^#[a-zа-яё0-9]{1,19}$', 'i');
 
 const formElement = document.querySelector('.img-upload__form');
 const uploadElement = formElement.querySelector('#upload-file');
 const overlayElement = formElement.querySelector('.img-upload__overlay');
-// const imgPreview = document.querySelector('.img-upload__preview img');
 const previewPopupCancelBtn = formElement.querySelector('#upload-cancel');
 const hashtagsElement = formElement.querySelector('.text__hashtags');
 const descriptionElement = formElement.querySelector('.text__description');
@@ -33,6 +33,8 @@ const closePreviewPopup = () => {
   overlayElement.classList.add('hidden');
   document.body.classList.remove('modal-open');
   formElement.reset();
+  pristineForHashtags.reset();
+  pristineForDescription.reset();
   document.removeEventListener('keydown', onDocumentKeydown);
 };
 
@@ -56,7 +58,9 @@ uploadElement.addEventListener('change', () => {
 });
 
 const validateHashtagsAmount = () => {
-  const hashtags = hashtagsElement.value.split(' ');
+  const hashtags = hashtagsElement.value
+    .trim()
+    .split(/\s+/);
   if (hashtags.length > MAX_HASHTAGS_AMOUNT) {
     return false;
   }
@@ -64,7 +68,9 @@ const validateHashtagsAmount = () => {
 };
 
 const validateHashtagsUniqueness = () => {
-  const hashtags = hashtagsElement.value.split(' ');
+  const hashtags = hashtagsElement.value
+    .trim()
+    .split(/\s+/);
   const filteredHashtags = hashtags.filter((element) => element !== '');
   if (filteredHashtags.length === 1) {
     return true;
@@ -82,29 +88,17 @@ const validateHashtagsUniqueness = () => {
 };
 
 const validateHashtag = () => {
-  const hashtags = hashtagsElement.value.split(' ');
-
-  const regexp = new RegExp('^#[a-zа-яё0-9]{1,19}$', 'i');
-  for (let i = 0; i < hashtags.length; i++) {
-    if (!regexp.test(hashtags[i])) {
-      return false;
-    }
-  }
-
-  return true;
+  const hashtags = hashtagsElement.value
+    .trim()
+    .split(/\s+/);
+  return hashtags.every((element) => REGEXP.test(element));
 };
 
 pristineForHashtags.addValidator(hashtagsElement, validateHashtagsAmount, 'Можно указать максимум 5 хештегов');
 pristineForHashtags.addValidator(hashtagsElement, validateHashtagsUniqueness, 'Хештеги не должны повторяться');
 pristineForHashtags.addValidator(hashtagsElement, validateHashtag, 'Введите корректный хештег');
 
-const validateDescription = () => {
-  if (descriptionElement.value.length > MAX_DESCRIPTION_LENGTH) {
-    return false;
-  }
-
-  return true;
-};
+const validateDescription = () => descriptionElement.value.length <= MAX_DESCRIPTION_LENGTH;
 
 pristineForDescription.addValidator(descriptionElement, validateDescription, 'Максимальная длина комментария 140 символов');
 
